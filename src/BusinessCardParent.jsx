4bea0f21c6a1c50bcaf214b2c8134c17c63e467a
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-09-21 20:24:06 +1000

    feat: state in parent pass as prop in display

--- src/BusinessCardParent.jsx
+++ src/BusinessCardParent.jsx
@@ -4,14 +4,26 @@ import BusinessCardDisplay from "./BusinessCardDisplay";
 class BusinessCardParent extends Component {
 	constructor(){
 		super();
+
+        this.state = {
+            editMode: false,
+            name: "Example name",
+            email: "example@email.com"
+        }
 	}
 
     render(){
-        return (
-            <div>
-                <BusinessCardDisplay />
-            </div>
-        )
+        if (this.state.editMode) {
+        }else {
+            return (
+                <div>
+                    <BusinessCardDisplay 
+                    email={this.state.email}
+                    name={this.state.name}/>
+                </div>
+            )
+        }
+
     }
 }
 export default BusinessCardParent;
