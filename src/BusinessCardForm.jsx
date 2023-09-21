64b839609884bb2661494b2743d470129d643761
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-09-21 21:01:42 +1000

    feat: add update state handler

--- src/BusinessCardForm.jsx
+++ src/BusinessCardForm.jsx
@@ -1,22 +1,33 @@
 import { Component } from "react";
 
+
 export default class BusinessCardForm extends Component {
-    constructor(props){
-        super(props);
+	constructor(props){
+		super(props);
 
-        this.state = {
-            name: this.props.name,
-            email: this.props.email,
-        }
-    }
+		this.state = {
+			name: this.props.name,
+			email: this.props.email
+		}
+	}
 
-    render(){
-        return (
-            <form>
-                {/* htmlFor instead of for */}
-                <label htmlFor="nameInput">Name:</label>
-                <input type="text" name="nameInput" id="nameInput" value={this.state.name}/>
-            </form>
-        )
-    }
+	handleChangeInput = (event) => {
+		// pass in the form input data 
+		// call the parent setState function 
+
+		this.props.setParentState(event.target.name, event.target.value);
+	}
+
+	render(){
+		return(
+			<form>
+				{/* htmlFor instead of for for the JSX compiler */}
+				<label htmlFor="name" >Name:</label>
+				<input type="text" name="name" id="nameInput" 
+				value={this.props.name} 
+				onChange={this.handleChangeInput}
+				/>
+			</form>
+		)
+	}
 }
--- src/BusinessCardParent.jsx
+++ src/BusinessCardParent.jsx
@@ -1,17 +1,20 @@
 import {Component} from "react";
+
 import BusinessCardDisplay from "./BusinessCardDisplay";
 import BusinessCardForm from "./BusinessCardForm";
 
-class BusinessCardParent extends Component {
+export default class BusinessCardParent extends Component {
 	constructor(){
 		super();
 
-        this.state = {
-            editMode: false,
-            name: "Example name",
-            email: "example@email.com"
-        }
+		this.state = {
+			editMode: false,
+			name: "Example name",
+			email:"example@email.com"
+		}
 	}
+
+	
 	
 	/**
 	 * Description placeholder
@@ -19,38 +22,41 @@ class BusinessCardParent extends Component {
 	 * @param stateKeyId - String, should match a key in this.state for BusinessCardParent.
 	 * @param newStateValue - String, should be the new value we want to set for the state key in BusinessCardParent.
 	 */
-    updateState=(stateKeyId, newStateValue) => {
-        // this.setState({
-        //     name: newStateValue
-        // })
+	updateState = (stateKeyId, newStateValue) => {
+		// this.setState({
+		// 	name: newStateValue
+		// })
+
 		this.setState({
 			[stateKeyId]: newStateValue
 		})
 
 		// updateState("name", "some new name");
 		// updateState("email", "some new email");
+	}
 
-    }
 
-    render(){
-        if (this.state.editMode) {
-            return(
-                <BusinessCardForm 
-                name={this.state.name} 
-                email={this.state.email} 
-                setParentState={this.updateState}/>
-            )
-        }else {
-            return (
-                <div>
-                    <BusinessCardDisplay 
-                    email={this.state.email}
-                    name={this.state.name}/>
-                </div>
-            )
-        }
+	render(){
+		if (this.state.editMode){
+			// if edit is true, show the form 
+			return(
+				<BusinessCardForm  
+				name={this.state.name} 
+				email={this.state.email} 
+				setParentState={this.updateState}
+				/>
+			)
 
-    }
+		} else {
+			return(
+				<div>
+					<BusinessCardDisplay
+					email={this.state.email}
+					name={this.state.name}
+					 
+					 />
+				</div>
+			)
+		}
+	}
 }
-export default BusinessCardParent;
-
