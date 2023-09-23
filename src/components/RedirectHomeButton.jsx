4daa7e4c5875d7af4d5ad50d84942874f9a2ae86
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-09-23 11:59:15 +1000

    feat: Redirect DRY component button

---
+++ src/components/RedirectHomeButton.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import { Navigate } from "react-router-dom";
+
+
+export default class RedirectHomeButton extends Component {
+    constructor (props){
+        super(props);
+
+        this.state = {
+            shouldRedirect: false
+        }
+    }
+    render(){
+        return(
+            <div>
+                <button onClick={() => this.setState({shouldRedirect: true})}>
+                    Return Home
+                </button>
+                {this.state.shouldRedirect && <Navigate to= "/" />}
+            </div>
+        )
+    }
+}
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,10 @@
+
+
 export default function ContactPage(props){
 	return(
 		<div>
 			<h1>Contact Us</h1>
+
 		</div>
 	)
 }
