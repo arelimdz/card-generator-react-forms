d5ff2df1e27f238f4e287b73af5d230e99486ea7
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-09-23 10:32:28 +1000

    feat: Add Router and Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import BusinessCardParent from './BusinessCardParent';
 
 function App() {
   return (
     <div className="App">
-      <BusinessCardParent />
-
 
     </div>
   );
